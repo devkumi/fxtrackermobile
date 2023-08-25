018fac6b8a868fb10271749cc1bef6ec53d20a67
Emma-legion <emmanuelagyeikumi@gmail.com>
2023-08-25 15:04:22 +0000

    Plain template for app

--- App.js
+++ App.js
@@ -1,10 +1,101 @@
+import { add, format } from 'date-fns';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+const initcapital = 9000;
+const rate = 0.065;
+const savings = 2500;
+const period = 6;
+
+const now = new Date();
+// const months = ["August", "September", "October", "November", "December", 'January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const saving = [3000, 8000, 2000, 3000, 5000, 7000, 2000, 2000];
+// const saving = [0, 0, 0, 0, 0, 0];
+const maturityDate = add(new Date(), { months: period });
+// console.log("Add 3 months to November", maturityDate);
+
+
+const capital = () => {
+  var capital = initcapital;
+
+  for (let index = 0; index < period; index++) {
+    if (saving.length < index + 1) {
+      var savings = 2000;
+    } else {
+      var savings = saving[index];
+    }
+
+    if (index === 0) {
+      capital += savings;
+    } else {
+      capital += capital * rate + savings;
+    }
+  }
+
+  console.log(`Profit: ${(capital - initcapital).toLocaleString()}`);
+  console.log(`capital: ${capital.toLocaleString()}`);
+
+  return capital;
+};
+
+
+
+const capitaldetails = () => {
+  var capital = initcapital;
+  var arrayOfObjects = [];
+  // const capitaldetails() = fs.readFileSync('data.json');
+
+  for (let index = 0; index < period; index++) {
+    var obj = {};
+    // console.log(`${months[index]}: ${(capital * rate).toLocaleString()} `)
+    const maturityDate = add(new Date(), { months: index + 1 });
+    let mon = add(new Date(), { months: index });
+    if (saving.length < index + 1) {
+      var savings = 2000;
+    } else {
+      var savings = saving[index];
+    }
+
+    if (index === 0) {
+      capital += savings;
+      obj["id"] = index;
+      obj["month"] = format(mon, "MMMM yy");
+      obj["initcapital"] = initcapital.toLocaleString();
+      obj["capitalM"] = capital.toLocaleString();
+      obj["contributions"] = savings;
+      obj["totalM"] = (capital - initcapital).toLocaleString();
+      obj["profitM"] = (capital * rate).toLocaleString();
+      obj["maturityDate"] = format(maturityDate, "dd/MM/yyyy");
+      arrayOfObjects.push(obj);
+    } else {
+      obj["totalM"] = (capital * rate + savings).toLocaleString();
+      capital += capital * rate + savings;
+
+      obj["capitalM"] = capital.toLocaleString();
+      obj["month"] = format(mon, "MMMM yy");
+      obj["id"] = index;
+      obj["profitM"] = (capital * rate).toLocaleString();
+      obj["initcapital"] = initcapital.toLocaleString();
+      obj["contributions"] = savings;
+      obj["maturityDate"] = format(maturityDate, "dd/MM/yyyy");
+      arrayOfObjects.push(obj);
+    }
+  }
+
+  console.log(`Profit: ${(capital - initcapital).toLocaleString()}`);
+  console.log(`capital: ${capital.toLocaleString()}`);
+  console.log(arrayOfObjects);
+
+  return arrayOfObjects;
+};
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Total: {capital().toLocaleString()}</Text>
+      <Text>Initcapital: {initcapital.toLocaleString()}</Text>
+      <Text>Profit: {(capital() - initcapital).toLocaleString()}</Text>
       <StatusBar style="auto" />
     </View>
   );
